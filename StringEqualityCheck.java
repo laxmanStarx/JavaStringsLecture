6ac9ecb73e7ae716b59ddb7ba2c6763791a7fec5
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-05-24 13:07:04 +0530

    String method

--- StringEqualityCheck.java
+++ StringEqualityCheck.java
@@ -4,25 +4,27 @@ public class StringEqualityCheck {
     {
         String str = "Love";
         String str1 = new String("Greffin");
-        System.out.print(str + " " + str1);
+        // System.out.print(str + " " + str1);
 
         // String str2 = "LoVe";
 
-        String str2 = new String("LoVe");
+        String str2 = new String("Love");
 
-        if(str.equalsIgnoreCase(str2)){
+        if(str.equals(str2)){
              System.out.println( "They both are same");
 
         }else{
             System.out.println("They are not same");
         }
 
-        // if(str == str2)
-        // {
-        //     System.out.println( "They both are same");
-        // }else{
-        //     System.out.println("They are not same");
-        // }
+        if(str == str2)
+        {
+            System.out.println( "They both are same");
+        }else{
+            System.out.println("They are not same");
+        }
+
+        System.out.println(str.charAt(3));
     }
     
 }
---
+++ StringMethods.java
@@ -0,0 +1,9 @@
+public class StringMethods {
+
+    public static void main(String[] args)
+    {
+        String str = "Sun rises in the east";
+        System.out.println(str.length());
+    }
+    
+}
