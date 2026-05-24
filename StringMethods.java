86e3e74bd07df12ef51705cc24ff8b9db1148735
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-05-24 17:28:04 +0530

    String method23

--- StringMethods.java
+++ StringMethods.java
@@ -6,6 +6,18 @@ public class StringMethods {
         System.out.println(str.length());
 
         System.out.println(str.charAt(5));
+
+        System.out.println(str.substring(2,11));
+
+        System.out.println(str.startsWith("Sun r"));
+         System.out.println(str.endsWith("the east"));
+
+         String str2 = "Mangoes";
+
+         String modify = str2.replace('M', 'D');
+
+         System.out.println(modify);
+
     }
     
 }
