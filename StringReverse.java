5fd5dba7226ced5010eaaeb9ea1b4542219fdd55
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-05-25 00:40:26 +0530

    String method23kk

---
+++ StringReverse.java
@@ -0,0 +1,54 @@
+public class StringReverse {
+
+    static boolean reversing(String str)
+    {
+
+
+        char name[] = str.toCharArray();
+
+                int start = 0;
+
+        int end = name.length- 1;
+
+        while(start < end){
+            // char temp = name[start];
+            // name[start] = name[end];
+            // name[end] = temp;
+
+            if(str.charAt(start)!= str.charAt(end))
+                return false;
+
+            start++;
+            end--;
+
+        }
+        // for(int ch = 0; ch<name.length; ch++){
+        //     System.out.print(name[ch] + " ");;
+        // }
+
+        return true;
+
+
+   
+    }
+
+    public static void main(String[] args)
+    {
+
+        String str = "racescar";
+
+  System.out.println(reversing(str));
+
+
+
+    
+
+       
+        
+
+      
+       
+
+    }
+    
+}
